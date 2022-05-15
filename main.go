0a69387c6823fd869c8443c1351ac0cf624fb445
ratbek <ratbek@pm.me>
2022-05-16 01:06:51 +0300

    Create HTTP Server that return 'Hello, World!' at root path

--- main.go
+++ main.go
@@ -1,8 +1,15 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+    "net/http"
+)
 
 func main() {
-    fmt.Println("Hello, World!")
+    http.HandleFunc("/", func (w http.ResponseWriter, r *http.Request) {
+        fmt.Fprintf(w, "Hello, World!")
+    })
+
+    http.ListenAndServe(":80", nil)
 }
 
