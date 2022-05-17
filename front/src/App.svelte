98b58e18527c4e48bda2f6982fdebc53d3e8d069
ratbek <ratbek@pm.me>
2022-05-17 22:16:15 +0300

    Add functionality to append new content to list of contents

--- front/src/App.svelte
+++ front/src/App.svelte
@@ -2,9 +2,23 @@
     import Block from './components/Block.svelte'
 
     let contents = []
+    let content = ''
+
+    function saveContent() {
+        if (!content) {
+            return
+        }
+
+        contents = [...contents, content]
+        content = ''
+    }
 </script>
 
 <main>
+    <form on:submit|preventDefault={saveContent}>
+        <input type="text" bind:value={content}/>
+        <input type="submit" value="Add"/>
+    </form>
     {#each contents as content, i}
         <Block content={content}/>
     {/each}
