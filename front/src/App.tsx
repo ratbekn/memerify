e1a964ddcffd9a33b06e3ce367394b4df2b86edf
ratbek <ratbek@pm.me>
2022-05-18 18:59:53 +0300

    Show the list of contents

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
+import Block from './components/Block'
+
 function App() {
+  const [contents, _] = useState<string[]>(['Bob', 'Alice', 'John'])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {contents.map((x, i) => <Block content={x} key={i}/>)}
     </div>
   );
 }
