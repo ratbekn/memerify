e4569b130bb57f7f1c0122eba182603a056065f8
ratbek <ratbek@pm.me>
2022-05-18 19:29:37 +0300

    Add functionality to append new content

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,17 +1,37 @@
-import React from 'react';
-import { useState } from 'react';
+import React, {ChangeEvent, FormEvent} from 'react';
+import {useState} from 'react';
 import './App.css';
 
-import Block from './components/Block'
+import Block from './components/Block';
 
 function App() {
-  const [contents, _] = useState<string[]>(['Bob', 'Alice', 'John'])
+    const [contents, setContents] = useState<string[]>([]);
+    const [content, setContent] = useState<string>('');
 
-  return (
-    <div className="App">
-      {contents.map((x, i) => <Block content={x} key={i}/>)}
-    </div>
-  );
+    const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+
+        if (!content) {
+            return;
+        }
+
+        setContents([...contents, content])
+        setContent('')
+    };
+
+    const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+        setContent(event.target.value);
+    };
+
+    return (
+        <div className="App">
+            <form onSubmit={handleSubmit}>
+                <input type="text" onChange={handleChange} value={content}/>
+                <input type="submit"/>
+            </form>
+            {contents.map((x, i) => <Block content={x} key={i}/>)}
+        </div>
+    );
 }
 
 export default App;
