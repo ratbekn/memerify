5e9c659ef0c59279a716f744ca6657f511227f20
ratbek <ratbek@pm.me>
2022-06-08 19:02:53 +0500

    Create Header component

---
+++ components/Header.tsx
@@ -0,0 +1,33 @@
+function Header() {
+    return (
+        <div className="flex-[2]
+                        border-r
+                        border-l
+                        border-[#38444d]
+                        text-white">
+            <div className="sticky
+                            top-0
+                            bg-[#15202b]
+                            z-10
+                            p-4
+                            flex
+                            justify-between
+                            items-center">
+                <div className="text-xl
+                                font-bold">
+                    Home
+                </div>
+                <div className="">
+                    <div>
+                        ratbek
+                    </div>
+                    <div>
+                        0xa559ee4ce4a6b43a568ef92e90b6fbc380c199850281e956d8b8100256357cd5
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Header;
