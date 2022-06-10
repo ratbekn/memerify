bfc075670d8ab42ee1af50887821cbc94e0fa1a8
ratbek <ratbek@pm.me>
2022-06-10 10:54:50 +0500

    Show all posts in feed

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -1,28 +1,73 @@
-import CreatePost from '../components/CreatePost'
-import Post from '../components/Post';
+import {useState, useEffect, useCallback} from 'react';
+import {ethers} from 'ethers';
 
-const posts = [
-    {
-        username: 'Bob',
-        content: 'Hello, World!'
-    },
-    {
-        username: 'Alice',
-        content: 'Hello, World!'
-    },
-    {
-        username: 'John',
-        content: 'Hello, World!'
-    }
-];
+import CreatePost from '../components/CreatePost';
+import Post from '../components/Post';
+import PostContract from '../contracts/PostStorage.json';
+
+type Post = {
+    id: number,
+    username: string,
+    content: string
+}
 
 function Feed() {
+    const [posts, setPosts] = useState<Post[]>([]);
+
+    const getPosts = useCallback(async () => {
+        try {
+            // @ts-ignore
+            const {ethereum} = window;
+
+            if (!ethereum) {
+                console.log('Can\'t find Ethereum object. Check if MetaMask installed or try using another browser');
+
+                return;
+            }
+
+            const provider = new ethers.providers.Web3Provider(ethereum);
+            await provider.send('eth_requestAccounts', []);
+            const signer = provider.getSigner();
+            const postContract = new ethers.Contract(
+                '0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0',
+                PostContract.abi,
+                signer
+            );
+
+            const allPosts = (await postContract.getPosts());
+
+            let newPosts: Post[] = [];
+            for (const newPost of allPosts) {
+                const response = await fetch(`/api/post/${newPost.content}`, {
+                    method: 'GET',
+                });
+
+                if (!response.ok) {
+                    throw new Error(`Error on retrieving post: ${response.status}`);
+                }
+
+                const {content} = await response.json();
+
+                newPosts = [...newPosts, {id: newPost.id, username: newPost.username, content: content}];
+            }
+
+            setPosts(newPosts);
+
+        } catch (error) {
+            console.log(`Error on loading all posts: ${error}`);
+        }
+    }, []);
+
+    useEffect(() => {
+        getPosts();
+    }, [getPosts]);
+
     return (
         <div>
-            <CreatePost/>
+            <CreatePost getPosts={getPosts}/>
             {
                 posts.map((post, index) =>
-                    <Post key={index} username={post.username} content={post.content}/>
+                    <Post key={post.id} username={post.username} content={post.content}/>
                 )
             }
         </div>
